6baaa8d5debde274342aafada9e68b1dc3153988
Oldflat <ducksalt+github@gmail.com>
2023-04-04 10:39:52 -0400

    efficiency WIP

--- py-the-leetcode/word_search.py
+++ py-the-leetcode/word_search.py
@@ -12,11 +12,15 @@ class Solution:
         self.board = []
         self.n = 0  # width
         self.m = 0  # height
+        self.vert_directions = {0: -1, 1: 0, 2: 1, 3: 0}
+        self.horiz_directions = {0: 0, 1: 1, 2: 0, 3: -1}
 
     def exist(self, board: List[List[str]], word: str) -> bool:
         self.board = board
         self.m = len(board)  # height
         self.n = len(board[0])  # width
+        if not self.quick_tests_pass(word):
+            return False
         for i in range(self.n):
             for j in range(self.m):
                 if board[j][i] == word[0]:
@@ -24,42 +28,12 @@ class Solution:
                         return True
         return False
 
-    def exist_recursive_right(self, word, start_coord, used_indices):
-        j, i = start_coord
-        next_coord = (j, i + 1)
-        if i == self.n - 1:
-            return False
-        if self.board[j][i + 1] != word[0] or next_coord in used_indices:
-            return False
-        more_used_indices = [next_coord] + used_indices
-        return self.exist_recursive(word, next_coord, more_used_indices)
+    def exist_recursive_direction(self, word, j, i, direction, used_indices):
+        next_coord = (j + self.vert_directions[direction], i + self.horiz_directions[direction])
 
-    def exist_recursive_left(self, word, start_coord, used_indices):
-        j, i = start_coord
-        next_coord = (j, i - 1)
-        if i == 0:
+        if next_coord[0] == -1 or next_coord[0] == self.m or next_coord[1] == -1 or next_coord[1] == self.n:
             return False
-        if self.board[j][i - 1] != word[0] or next_coord in used_indices:
-            return False
-        more_used_indices = [next_coord] + used_indices
-        return self.exist_recursive(word, next_coord, more_used_indices)
-
-    def exist_recursive_up(self, word, start_coord, used_indices):
-        j, i = start_coord
-        next_coord = (j - 1, i)
-        if j == 0:
-            return False
-        if self.board[j - 1][i] != word[0] or next_coord in used_indices:
-            return False
-        more_used_indices = [next_coord] + used_indices
-        return self.exist_recursive(word, next_coord, more_used_indices)
-
-    def exist_recursive_down(self, word, start_coord, used_indices):
-        j, i = start_coord
-        next_coord = (j + 1, i)
-        if j == self.m - 1:
-            return False
-        if self.board[j + 1][i] != word[0] or next_coord in used_indices:
+        if self.board[next_coord[0]][next_coord[1]] != word[0] or next_coord in used_indices:
             return False
         more_used_indices = [next_coord] + used_indices
         return self.exist_recursive(word, next_coord, more_used_indices)
@@ -72,10 +46,21 @@ class Solution:
         if len(word) == 1 and self.board[j][i] == word[0]:
             print(f"found the word, used_indices: {used_indices[::-1]}")
             return True
-        return self.exist_recursive_up(word[1:], start_coord, used_indices) or \
-            self.exist_recursive_down(word[1:], start_coord, used_indices) or \
-            self.exist_recursive_right(word[1:], start_coord, used_indices) or \
-            self.exist_recursive_left(word[1:], start_coord, used_indices)
+        if not self.exist_recursive_direction(word[1:], j, i, 0, used_indices):
+            if not self.exist_recursive_direction(word[1:], j, i, 1, used_indices):
+                if not self.exist_recursive_direction(word[1:], j, i, 2, used_indices):
+                    if not self.exist_recursive_direction(word[1:], j, i, 3, used_indices):
+                        return False
+        return True
+
+    def quick_tests_pass(self, word):
+        board_letter_list = [letter for row in self.board for letter in row]
+        if len([word_letter for word_letter in word if word_letter not in board_letter_list]):
+            return False
+        if len(word) > len(board_letter_list):
+            return False
+        return True
+
 
 
 def get_word(line):
