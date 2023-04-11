fa0fe11aa19637410356010126f005327191d42e
Oldflat <ducksalt+github@gmail.com>
2023-04-11 19:46:41 -0400

    coin change 2, WIP III

--- py-the-leetcode/coin_change_2.py
+++ py-the-leetcode/coin_change_2.py
@@ -1,5 +1,8 @@
 """
     518. Coin Change II
+
+    https://leetcode.com/problems/coin-change-ii/
+
     Medium
     Companies
 
@@ -16,7 +19,7 @@
     Note: test cases should include,
     1. Primes, with a nonzero result (change can be made). E.g., change(101, [2, 3, 5, 7, 11])
     2. Zero result. E.g., change(100, [51, 52, 54, 55, 56, 66])
-    3. A nice long case, like the project euler one that has 72,000 ways of making change.
+    3. A nice long case, like the project euler one that has 73682 ways of making change.
         E.g., change(200, [200, 100, 50, 20, 10, 5, 2, 1])
 
 """
@@ -25,33 +28,32 @@ import ast
 
 
 class Solution:
-    def __init__(self):
-        self.coins = []
-        self.amount = 0
-        self.combos = []
-
     def change(self, amount: int, coins: List[int]) -> int:
-        self.amount = amount
-        self.coins = coins
-        self.coins.sort(reverse=True)
-        base_coins = {c: 0 for c in coins}
+        coins.sort(reverse=True)
+        return self.change_iter(amount, coins)
 
-        self.combos = [base_coins]
+    def change_rec(self, amount: int, coins: List[int]) -> int:
+        if len(coins) == 1:
+            return 1 if amount % coins[0] == 0 else 0
+        return sum([self.change_rec(amount - coins[0] * i, coins[1:]) for i in range(int(amount / coins[0]) + 1)])
 
-        for i in range(len(coins)):
-            """
-                Start of loop: Every coin in coins up to 'i' should be REPRESENTED in self.combos
-                Initially: self.combos = [{base_coins}]
-            """
-            coin_value = coins[i]
-            for combo in self.combos:
+    def change_iter(self, amount: int, coins: List[int]) -> int:
+        """We'll store some intermediate values to speed calculation"""
+        change_count = 0
+        lookup = {}
+        coin_kinds = len(coins)
 
-
-                new_combos = []
-
-                self.combos = new_combos
-
-        return len(self.combos)
+        coin_index = coin_kinds - 1
+        lookup[coin_index] = [1 if i % coins[coin_index] == 0 and i != 0 else 0 for i in range(amount + 1)]
+        print(f"coin: {coins[coin_index]}, amount = {amount}, lookup: {lookup[coin_index]}")
+        coin_index -= 1
+        while coin_index >= 0:
+            last_one = lookup[coin_index + 1]
+            this_one = [(1 if i % coins[coin_index] == 0 and i != 0 else 0) + lookup[coin_index + 1][i] for i in range(amount + 1)]
+            lookup[coin_index] = [sum([last_one[j] + this_one[i-j] for j in range(i)]) for i in range(amount+1)]
+            print(f"coin: {coins[coin_index]}, amount = {amount}, lookup: {lookup[coin_index]}")
+            coin_index -= 1
+        return change_count
 
 
 def main():
@@ -64,7 +66,7 @@ def main():
         coin_changer.amount, coin_changer.coins = ast.literal_eval(line)
         change_result = coin_changer.change(coin_changer.amount, coin_changer.coins)
         print(f"Let's change these coins! Amount: {coin_changer.amount}, Coins: {coin_changer.coins}, " +
-                f"Ways of change: {change_result}")
+              f"Ways of change: {change_result}")
 
 
 if __name__ == '__main__':
